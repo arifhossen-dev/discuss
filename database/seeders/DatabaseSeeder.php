b1520349a660891a11dea8ed91b7dbcd7f3f6797
arifhossen-dev <ahak.bsl@gmail.com>
2024-01-15 02:09:21 +0600

    DatabaseSeeder prepared with Post and Comment Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $post = Post::factory(200)->recycle($user)->create();
+
+        $comment = Comment::factory(100)->recycle($user)->recycle($post)->create();
+
+        User::factory()
+            ->has(Post::factory(45))
+            ->has(Comment::factory(120)->recycle($post))
+            ->create([
+                'name' => 'Arif Hossen',
+                'email' => 'test@mail.com',
+            ]);
     }
 }
