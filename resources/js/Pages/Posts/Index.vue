8d75026b26a6e70c048dc82cbb7ea313d48643e2
arifhossen-dev <ahak.bsl@gmail.com>
2024-01-16 01:54:28 +0600

    Post Index according to The Post IndexTest

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Container from '@/Components/Container.vue';
+
+defineProps(['posts'])
+</script>
+
+<template>
+  <AppLayout>
+    <Container>
+      <ul class="divide-y">
+        <li v-for="(post, index) in posts" :key="index" class="px-2 py-4">
+          <span class="font-bold text-large">{{ post.title }}</span>
+        </li>
+      </ul>
+    </Container>
+  </AppLayout>
+</template>
