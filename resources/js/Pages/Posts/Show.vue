2f0a568773a032a3765ebd746a4f6c953a614392
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-01 18:33:36 +0600

    It passes comments to the view test passed

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -2,25 +2,40 @@
 
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
-import {formatDistance, parseISO} from "date-fns";
 import {computed} from "vue";
+import Pagination from "@/Components/Pagination.vue";
+import {relativeDate} from "@/Utilities/date.js";
+import Comment from "@/Components/Comment.vue";
 
-const prop = defineProps(['post'])
+const prop = defineProps(['post', 'comments'])
 
-const formattedDate = computed(()=>formatDistance(parseISO(prop.post.created_at),new Date()))
+const formattedDate = computed(() => relativeDate(prop.post.created_at));
 </script>
 
 <template>
     <AppLayout>
         <container>
-            <h2 class="text-2xl font-bold">{{ post.title}}</h2>
-            <span class="text-sm text-gray-500">{{ formattedDate}} ago by <span class="font-bold">{{post.user.name}}</span></span>
+            <h2 class="text-2xl font-bold">{{ post.title }}</h2>
+            <span class="text-sm text-gray-500">{{ formattedDate }} ago by <span
+                class="font-bold">{{ post.user.name }}</span></span>
 
             <article class="mt-4">
                 <pre class="whitespace-pre-wrap font-sans">
-                {{ post.body}}
+                {{ post.body }}
                 </pre>
             </article>
+
+            <div>
+                <h2 class="border-b-4 font-bold text-2xl">Comments</h2>
+
+                <ul class="divide-y">
+                    <li v-for="(comment, index) in comments.data" :key="index" class="px-2 py-4 flex flex-col">
+                        <Comment :comment="comment"/>
+                        
+                    </li>
+                </ul>
+                <Pagination :meta="comments.meta"/>
+            </div>
         </container>
     </AppLayout>
 </template>
