34fbfdaf3a3f89b76c4a6b35a84625f528a288a9
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-01 18:32:34 +0600

    Comment vue component created

---
+++ resources/js/Components/Comment.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {relativeDate} from "@/Utilities/date.js";
+
+defineProps(['comment'])
+</script>
+
+<template>
+    <div class="sm:flex">
+        <div class="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+            <img :src="comment.user.profile_photo_url" class="h-10 wh-10 rounded-full">
+        </div>
+        <div>
+            <p class="mt-1">{{comment.body}}</p>
+            <span class="text-sm text-gray-500">{{ relativeDate(comment.created_at) }} ago by <span
+                class="font-bold">{{ comment.user.name }}</span></span>
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
