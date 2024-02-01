c0e006e054817528e7706422cc174e92050a2fd2
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-01 23:59:46 +0600

    Comment store is working according to the test

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -26,9 +27,20 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
     {
-        //
+        $request->validate([
+            'body' => ['required','string','max:2500']
+        ]);
+
+        $comment = Comment::make($request->all());
+
+        $comment->user()->associate($request->user());
+        $comment->post()->associate($post);
+
+        $comment->save();
+
+        return to_route('posts.show', $post);
     }
 
     /**
