f3c113b7d4a6b43cd2efe645cd741d7615edda6c
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-02 00:55:45 +0600

    Refactor

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -29,16 +29,12 @@ class CommentController extends Controller
      */
     public function store(Request $request, Post $post)
     {
-        $request->validate([
-            'body' => ['required','string','max:2500']
-        ]);
+        $data = $request->validate(['body' => ['required', 'string', 'max:2500']]);
 
-        $comment = Comment::make($request->all());
-
-        $comment->user()->associate($request->user());
-        $comment->post()->associate($post);
-
-        $comment->save();
+        Comment::make($data)
+            ->user()->associate($request->user())
+            ->post()->associate($post)
+            ->save();
 
         return to_route('posts.show', $post);
     }
