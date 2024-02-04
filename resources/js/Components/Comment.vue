5419b42bc7fc5a60e800d2613ecc10fe7c95ff13
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-05 00:16:18 +0600

    Delete comment on front-end working fine with owner check

--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -1,7 +1,15 @@
 <script setup>
 import {relativeDate} from "@/Utilities/date.js";
+import {router, usePage} from "@inertiajs/vue3";
+import {computed} from "vue";
 
-defineProps(['comment'])
+const props = defineProps(['comment'])
+
+const deleteComment = () => router.delete(route('comments.destroy', props.comment.id), {
+    preserveScroll: true,
+})
+
+const canDelete = computed(() => props.comment.user.id === usePage().props.auth.user.id);
 </script>
 
 <template>
@@ -10,9 +18,15 @@ defineProps(['comment'])
             <img :src="comment.user.profile_photo_url" class="h-10 wh-10 rounded-full">
         </div>
         <div>
-            <p class="mt-1 break-all">{{comment.body}}</p>
+            <p class="mt-1 break-all">{{ comment.body }}</p>
             <span class="text-sm text-gray-500">{{ relativeDate(comment.created_at) }} ago by <span
                 class="font-bold">{{ comment.user.name }}</span></span>
+            <div v-if="canDelete" class="mt-1">
+                <form @submit.prevent="deleteComment">
+                    <button type="submit">Delete</button>
+                </form>
+
+            </div>
         </div>
 
     </div>
