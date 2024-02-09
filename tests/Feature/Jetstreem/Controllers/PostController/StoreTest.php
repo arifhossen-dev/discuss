b0da91250560cae1f1ce0a594f822e4ccae40794
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-09 21:47:28 +0600

    Store post test created with possible test assertion

---
+++ tests/Feature/Jetstreem/Controllers/PostController/StoreTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\post;
+
+beforeEach(function () {
+    $this->validData = [
+        'title' => 'Test title',
+        'body' => 'This is the test body of the post',
+    ];
+});
+
+it('requires authentication', function () {
+    post(route('posts.store'))
+        ->assertRedirect(route('login'));
+});
+
+it('store a post', function () {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->post(route('posts.store'), $this->validData);
+
+    $this->assertDatabaseHas(Post::class, [
+        ...$this->validData,
+        'user_id' => $user->id,
+    ]);
+});
+
+it('redirects to the post show page', function () {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->post(route('posts.store'), $this->validData)
+        ->assertRedirect(route('posts.show', Post::latest('id')->first()));
+});
+
+it('requires valid data', function (array $badData, array|string $errors) {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->post(route('posts.store'), [...$this->validData, ...$badData])
+        ->assertInvalid($errors);
+})->with([
+    [['title' => null], 'title'],
+    [['title' => 1], 'title'],
+    [['title' => 1.5], 'title'],
+    [['title' => true], 'title'],
+    [['title' => str_repeat('a', 221)], 'title'],
+    [['title' => str_repeat('a', 9)], 'title'],
+    [['body' => null], 'body'],
+    [['body' => 1], 'body'],
+    [['body' => 1.5], 'body'],
+    [['body' => true], 'body'],
+    [['body' => str_repeat('a', 10_001)], 'body'],
+    [['body' => str_repeat('a', 99)], 'body'],
+]);
