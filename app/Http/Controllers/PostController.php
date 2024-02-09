f771f0ff037d57d02521d5c87f3755e7ad1b6e64
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-10 05:00:37 +0600

    PostPolicy added in PostResource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,11 @@ use Inertia\Response;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Post::class);
+    }
+
     /**
      * Display a listing of the resource.
      */
