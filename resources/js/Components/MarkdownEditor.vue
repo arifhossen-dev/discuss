415e14535148959062114894b49f55efd24b6bef
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-26 23:58:35 +0600

    Remix Icon added in MarkdownEditor to format it

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -3,6 +3,7 @@ import {EditorContent, useEditor} from "@tiptap/vue-3";
 import {StarterKit} from "@tiptap/starter-kit";
 import {watch} from "vue";
 import {Markdown} from "tiptap-markdown";
+import 'remixicon/fonts/remixicon.css'
 
 const props = defineProps({
     modelValue: ''
@@ -32,6 +33,33 @@ watch(() => props.modelValue, (value) => {
 
 <template>
     <div class="bg-white rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm mb-3">
+        <menu class="flex divide-x border-b">
+            <li>
+                <button class="px-3 py-2 rounded-tl-md hover:bg-gray-200" type="button">
+                    <i class="ri-bold"></i>
+                </button>
+            </li>
+            <li>
+                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                    <i class="ri-italic"></i>
+                </button>
+            </li>
+            <li>
+                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                    <i class="ri-strikethrough"></i>
+                </button>
+            </li>
+            <li>
+                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                    <i class="ri-list-check"></i>
+                </button>
+            </li>
+            <li>
+                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                    <i class="ri-list-ordered"></i>
+                </button>
+            </li>
+        </menu>
         <EditorContent :editor="editor"/>
     </div>
 </template>
