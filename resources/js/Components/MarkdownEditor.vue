dbfeb8f2082be67814fbb05effa42c872fcc770e
arifhossen-dev <ahak.bsl@gmail.com>
2024-02-27 01:06:28 +0600

    Menu buttons added to MarkdownEditor for general users who don't know markdown

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -3,7 +3,8 @@ import {EditorContent, useEditor} from "@tiptap/vue-3";
 import {StarterKit} from "@tiptap/starter-kit";
 import {watch} from "vue";
 import {Markdown} from "tiptap-markdown";
-import 'remixicon/fonts/remixicon.css'
+import 'remixicon/fonts/remixicon.css';
+import Link from '@tiptap/extension-link';
 
 const props = defineProps({
     modelValue: ''
@@ -12,7 +13,12 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue'])
 const editor = useEditor({
     extensions: [
-        StarterKit,
+        StarterKit.configure({
+            heading: {
+                levels: [2, 3, 4],
+            },
+        }),
+        Link,
         Markdown
     ],
     editorProps: {
@@ -29,34 +35,113 @@ watch(() => props.modelValue, (value) => {
     }
     editor.value?.commands.setContent(value);
 }, {immediate: true})
+
+const promptUserForHref = () => {
+    if (editor.value?.isActive('link')) {
+        return editor.value?.chain().unsetLink().run();
+    }
+    const href = prompt('Whre do you want to link to?');
+
+    if (!href) {
+        return editor.value.chain().focus().run();
+    }
+
+    return editor.value?.chain().focus().setLink({href}).run();
+}
 </script>
 
 <template>
-    <div class="bg-white rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm mb-3">
+    <div v-if="editor"
+         class="bg-white rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm mb-3">
         <menu class="flex divide-x border-b">
             <li>
-                <button class="px-3 py-2 rounded-tl-md hover:bg-gray-200" type="button">
+                <button
+                    :class="[editor.isActive('bold')?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2 rounded-tl-md "
+                    type="button"
+                    @click="()=> editor.chain().focus().toggleBold().run()"
+                >
                     <i class="ri-bold"></i>
                 </button>
             </li>
             <li>
-                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                <button
+                    :class="[editor.isActive('italic')?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="()=> editor.chain().focus().toggleItalic().run()"
+                >
                     <i class="ri-italic"></i>
                 </button>
             </li>
             <li>
-                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                <button
+                    :class="[editor.isActive('stick')?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="()=> editor.chain().focus().toggleStrike().run()"
+                >
                     <i class="ri-strikethrough"></i>
                 </button>
             </li>
             <li>
-                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                <button
+                    :class="[editor.isActive('bulletList')?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="()=> editor.chain().focus().toggleBulletList().run()"
+                >
                     <i class="ri-list-check"></i>
                 </button>
             </li>
             <li>
-                <button class="px-3 py-2 hover:bg-gray-200" type="button">
+                <button
+                    :class="[editor.isActive('orderedList')?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="()=> editor.chain().focus().toggleOrderedList().run()"
+                >
                     <i class="ri-list-ordered"></i>
+                </button>
+            </li>
+            <li>
+                <button
+                    :class="[editor.isActive('link')?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="promptUserForHref"
+                >
+                    <i class="ri-link"></i>
+                </button>
+            </li>
+            <li>
+                <button
+                    :class="[editor.isActive('heading', { level: 2 })?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+                >
+                    <i class="ri-h-1"></i>
+                </button>
+            </li>
+            <li>
+                <button
+                    :class="[editor.isActive('heading', { level: 3 })?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+                >
+                    <i class="ri-h-2"></i>
+                </button>
+            </li>
+            <li>
+                <button
+                    :class="[editor.isActive('heading', { level: 4 })?'bg-indigo-500 text-white': 'hover:bg-gray-200']"
+                    class="px-3 py-2"
+                    type="button"
+                    @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+                >
+                    <i class="ri-h-3"></i>
                 </button>
             </li>
         </menu>
